1f56c994a644a27dc30ca45d2148dae00fc35a94
bulatsabirow <90245760+bulatsabirow@users.noreply.github.com>
2022-02-03 19:50:21 +0300

    Create database.py

---
+++ database.py
@@ -0,0 +1,32 @@
+import sqlite3
+bd = sqlite3.connect('2048.sqlite')
+
+cur = bd.cursor()
+cur.execute("""
+create table if not exists RECORDS (
+    name text,
+    score integer)
+
+""")
+
+def insert_result(name, score):
+    cur.execute("""
+    insert into RECORDS values (? , ?)
+    """,(name, score))
+    bd.commit()
+
+def get_best():
+    cur.execute("""
+    SELECT  name, max(score) score from RECORDS
+    GROUP by name
+    ORDER by score DESC
+    limit 3
+    """)
+    return cur.fetchall()
+
+
+
+
+
+
+
